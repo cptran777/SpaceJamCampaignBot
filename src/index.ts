0202e84e8506663e375385a1ee81ee9bcfbe1176
Charlie Tran <cptran14@gmail.com>
2021-08-26 00:56:23 -0700

    Add temporary express app to solve heroku crash

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,16 @@ client.on('interactionCreate', async interaction => {
   }
 });
  */
+
+// We are creating an express server to stop heroku from killing the app
+// Can remove once we change to a worker dyno
+// Or maybe we'll just keep this and have a web interface too
+const express = require("express");
+const app = express();
+
+const http = require("http").createServer(app);
+
+const port = process.env.PORT || 3005;
+http.listen(port, () => {
+  console.log("Listening on port", port);
+});
